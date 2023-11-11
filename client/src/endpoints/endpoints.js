de28c5bdc96e9e1b12018160c1912ff3355c2af6
grishanych <alsolo2011@gmail.com>
2023-11-11 18:10:10 +0200

    change endpoints

--- client/src/endpoints/endpoints.js
+++ client/src/endpoints/endpoints.js
@@ -1,5 +1,5 @@
-export const LOGIN_URL = "https://final-donate-fe-be-7hn5.vercel.app/api/customers/login";
-export const REGISTRATION_URL = "https://final-donate-fe-be-7hn5.vercel.app/api/customers";
-export const SUBSCRIBE_URL = "https://final-donate-fe-be-7hn5.vercel.app/api/subscribers";
-export const GET_PRODUCTS_URL = "https://final-donate-fe-be-7hn5.vercel.app/api/products";
-export const GET_FAVORITES = "https://final-donate-fe-be-7hn5.vercel.app/api/customers/customer";
+export const LOGIN_URL = "https://test-server-2-drab.vercel.app/customers/login";
+export const REGISTRATION_URL = "https://test-server-2-drab.vercel.app/api/customers";
+export const SUBSCRIBE_URL = "https://test-server-2-drab.vercel.app/api/subscribers";
+export const GET_PRODUCTS_URL = "https://test-server-2-drab.vercel.app/api/products";
+export const GET_FAVORITES = "https://test-server-2-drab.vercel.app/api/customers/customer";
