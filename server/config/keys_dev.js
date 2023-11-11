067fd488ecd3b4a53546fbaf664644004f52dfeb
grishanych <alsolo2011@gmail.com>
2023-11-11 17:56:47 +0200

    new com

--- server/config/keys_dev.js
+++ server/config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-  'final-donate-fe-be-uvni.vercel.app/db',
+  'mongodb+srv://danitfinalproject:q05A6fyLhr0FxXYX@online-shop-cluster.pesezjh.mongodb.net/db',
   secretOrKey: 'secret',
 };
--- server/index.js
+++ server/index.js
@@ -87,21 +87,3 @@ if (process.env.NODE_ENV === 'production') {
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
-
-// const express = require('express');
-
-// const app = express()
-// const PORT = 8000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World')
-// })
-
-// app.get('/about', (req, res) => {
-//   res.send('About route 🎉 ')
-// })
-
-// app.listen(PORT, () => {
-//   console.log(`✅ Server is running on port ${PORT}`);
-// })
